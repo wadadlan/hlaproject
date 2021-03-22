a50ddc48424c1bd1a4fb0743ac6c615d301eca55
wadadlan <35949880+wadadlan@users.noreply.github.com>
2021-03-22 12:04:41 +0300

    upload hala file

---
+++ contactus.php
@@ -0,0 +1,84 @@
+    
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>اتصال</h2>
+          <p>اتواصل معنا</p>
+        </div>
+      </div>
+
+      <div data-aos="fade-up">
+        
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://maps.google.com/maps?q=%D9%85%D9%86%D8%AF%D9%8A%20%D9%85%D8%B3%D9%84%D9%8A&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+      <div class="container" data-aos="fade-up">
+
+        <div class="row mt-5">
+
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>الموقع:</h4>
+                <p>Sayed Abdul Rahman Ave, الخرطوم، السودان</p>
+              </div>
+
+              <div class="open-hours">
+                <i class="bi bi-clock"></i>
+                <h4>ساعات العمل:</h4>
+                <p>
+                 كل ايام الاسبوع:<br>
+                  8:00 ص - 2300 م
+                </p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>الايميل:</h4>
+                <p>info@example.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>اتصال:</h4>
+                <p>0910006314</p>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0">
+
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="الاسم" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="الايميل" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="الموضوع" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="8" placeholder="الرساله" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">ارسال </button></div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
